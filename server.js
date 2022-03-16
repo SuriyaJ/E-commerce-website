d46a655e81b7267d6b3eba6e25f70bbe3ad12487
SuriyaJ <suriyajaganathan42@gmail.com>
2022-03-16 23:54:58 +0530

    changed server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 const URL = `mongodb+srv://${username}:${password}@cluster0.4voqu.mongodb.net/mydb?retryWrites=true&w=majority`;
-Connection(process.env.MONGO_URI || URL);
+Connection(process.env.MONGODB_URI || URL);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('client/build'));
