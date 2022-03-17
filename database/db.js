3fe23ffa9e37493a3e41d63d5e30381bb20c4758
SuriyaJ <suriyajaganathan42@gmail.com>
2022-03-17 14:50:05 +0530

    changed git url

--- database/db.js
+++ database/db.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
-
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const URL = `mongodb://${username}:${password}@cluster0.4voqu.mongodb.net/mydb?retryWrites=true&w=majority`;
 const Connection = async(URL) => {
     // const URL = `mongodb://${username}:${password}@ecommerceweb-shard-00-00.fdvft.mongodb.net:27017,ecommerceweb-shard-00-01.fdvft.mongodb.net:27017,ecommerceweb-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-8a6bhp-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
