3fbe407a0188f877a13d249d8655ba2df1fea9c1
SuriyaJ <suriyajaganathan42@gmail.com>
2022-03-17 14:02:09 +0530

    changed git url

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
-const URL = `mongodb+srv://suriya:suriya@58@cluster0.4voqu.mongodb.net/mydb?retryWrites=true&w=majority`;
+const URL = `mongodb://suriya:suriya@58@cluster0.4voqu.mongodb.net/mydb?retryWrites=true&w=majority`;
 Connection(process.env.MONGODB_URI || URL);
 
 if (process.env.NODE_ENV === "production") {
