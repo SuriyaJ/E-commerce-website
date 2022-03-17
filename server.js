3137c0a228ad4349ccceb101969b14f9e365d3a7
SuriyaJ <suriyajaganathan42@gmail.com>
2022-03-17 13:14:55 +0530

    changed git url

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ Connection(process.env.MONGODB_URI || URL);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/client/build")));
+
 
     app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "/client/build"))
+        app.use(express.static(path.join(__dirname, "/client/build")))
     );
 } else {
     app.get("/", (req, res) => {
