e6d45d7fb2a32dadf003625d696502b54745d3f9
SuriyaJ <suriyajaganathan42@gmail.com>
2022-03-24 12:05:42 +0530

    hello

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ paytmParams['MID'] = process.env.PAYTM_MID,
     paytmParams['ORDER_ID'] = uuid(),
     paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
     paytmParams['TXN_AMOUNT'] = '100',
-    paytmParams['CALLBACK_URL'] = 'http://localhost:8080/callback'
+    paytmParams['CALLBACK_URL'] = 'https://localhost:8080/callback'
 paytmParams['EMAIL'] = 'codeforinterview01@gmail.com'
 paytmParams['MOBILE_NO'] = '1234567852'
